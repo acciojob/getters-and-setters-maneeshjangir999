ff43c88951c5429850f6a58895db5fb29907a8eb
Maneesh Jangir <122728056+maneeshjangir999@users.noreply.github.com>
2025-02-24 21:48:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-//complete this code
 class Person {
-
-	constructor(name,age){
-		this.name = name
-		this.age = age
-	}
-	get name(){
-		return this.name
-	}
-	set age(umra){
-		this.age = umra
-	} 
-	
+    constructor(name, age) {
+        this._name = name; // Use _name to avoid shadowing the getter
+        this.age = age;
+    }
+    get name() {
+        return this._name;
+    }
+    set age(umra) {
+        this.age = umra;
+    }
 }
 
 class Student extends Person {
-	let study = ()=>{
-		console.log(`${this.name} is studying`)
-	}
-	
+    study = () => {
+        console.log(`${this.name} is studying`);
+    }
 }
 
 class Teacher extends Person {
-	let tech = ()=>{
-		console.log(`${this.name} is teaching`)
-	}
-	
+    teach = () => {
+        console.log(`${this.name} is teaching`);
+    }
 }
 
-let obj = new Person("Maneesh",21)
- console.log(obj.name)
+let obj = new Person("John", 25);
+console.log(obj.name);
+obj.age = 30;
+let obj1 = new Student("Alice", 30); // Changed name to avoid console output conflicts.
+obj1.study();
+let obj2 = new Teacher("Bob", 30); // Changed name to avoid console output conflicts.
+obj2.teach();
 
 // Do not change the code below this line
 window.Person = Person;
