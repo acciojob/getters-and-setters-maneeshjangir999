84bae5e2a629fde21a6d7e80bb3a6b2618d4ee4c
Maneesh Jangir <122728056+maneeshjangir999@users.noreply.github.com>
2025-02-24 21:29:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Person {}
+class Person {
 
-class Student extends Person {}
+	constructor(name,age){
+		this.name = name
+		this.age = age
+	}
+	get name(){
+		return this.name
+	}
+	set age(umra){
+		this.age = umra
+	} 
+	
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	let study = ()=>{
+		console.log(`${this.name} is studying`)
+	}
+	
+}
+
+class Teacher extends Person {
+	let tech = ()=>{
+		console.log(`${this.name} is teaching`)
+	}
+	
+}
+
+let obj = new Person("Maneesh",21)
+ console.log(obj.name)
 
 // Do not change the code below this line
 window.Person = Person;
